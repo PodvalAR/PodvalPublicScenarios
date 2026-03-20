f319ab8b609f0bd9971e50e38fa70d738362f351
Vladislav Sazonov <vladsazonov@hotmail.com>
2026-03-20 13:16:41 +0300

    remove bad script file

--- scripts/Game/Test.c
+++
@@ -1,9 +0,0 @@
-class MyTestComponentClass : ScriptComponentClass {};
-
-class MyTestComponent : ScriptComponent {
-	override void OnPostInit(IEntity owner) {
-		if (!GetGame().InPlayMode())
-			return;
-		super.OnPostInit(owner);
-	};
-};
